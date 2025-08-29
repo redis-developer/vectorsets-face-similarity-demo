f9b9c73444b87c5bf22c9a48f36ec85b56c1a508
PrasanKumar <prasankumar93@gmail.com>
2025-08-29 21:08:11 +0530

    MainPanel comp added

--- frontend/src/components/MainPanel/MainPanel.tsx
+++ frontend/src/components/MainPanel/MainPanel.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import styles from './MainPanel.module.scss'
+import Header from './Header/Header'
+import SearchBar from './SearchBar/SearchBar'
+import NearestMatchResult from './NearestMatchResult/NearestMatchResult'
+import OtherMatchResults from './OtherMatchResults/OtherMatchResults'
+
+const PAGE_LABELS = {
+    title: 'ACTOR SIMILARITY SEARCH'
+} as const
 
 const MainPanel: React.FC = () => {
     return (
         <div className={styles.mainPanel}>
-            {/* Component content will go here */}
+            <Header title={PAGE_LABELS.title} />
+            <SearchBar />
+            <NearestMatchResult />
+            <OtherMatchResults />
         </div>
     )
 }
